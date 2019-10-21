a025e19177ddeb0484844b0a03f1ec7626150d19
Gonzalo <gonzacalleja@gmail.com>
2019-10-21 10:48:27 +0200

    Exerxcise3 commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         public static void main(String [] args) {
             double d = 0.0;
 
-            while (d != 1.0) {
+            while (d - 1.0 < 0.0000001) {
                 d += 0.1;
             }
 
